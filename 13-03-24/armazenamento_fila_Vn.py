f4b597c221ead583c22bbbd8511f0b141cfd25b3
Ricardo Alexandre <ricardoalexandr_@hotmail.com>
2024-03-13 19:27:22 +0000

    change, lógica para armazenamento fila com N vértices

--- 13-03-24/armazenamento_fila_Vn.py
+++ 13-03-24/armazenamento_fila_Vn.py
@@ -1,3 +1,39 @@
 import networkx as nx
-#Crio um novo objeto Grafo com o nome G
-G = nx.Graph()
+
+def criar_grafo_lista_adjacencia(n):
+    grafo = {}
+    for vertice in range(n):
+        grafo[vertice] = []
+    return grafo
+
+def visualizar_grafo(grafo):
+    for vertice, adjacencias in grafo.items():
+        for adjacente in adjacencias:
+            print(f'Aresta: ({vertice}, {adjacente})')
+
+def vertices_adjacentes(grafo, vertice):
+    return grafo[vertice]
+
+def existe_aresta(grafo, v1, v2):
+    return v2 in grafo[v1] or v1 in grafo[v2]
+
+def grau_vertice(grafo, vertice):
+    return len(grafo[vertice])
+
+def verificar_regularidade(grafo):
+    grau = grau_vertice(grafo, 0)
+    for vertice in grafo:
+        if grau_vertice(grafo, vertice) != grau:
+            return False
+    return True
+
+grafo = criar_grafo_lista_adjacencia(5)
+
+grafo[0] = [1, 2]
+grafo[1] = [0, 3]
+grafo[2] = [0, 4]
+grafo[3] = [1]
+grafo[4] = [2]
+
+visualizar_grafo(grafo)
+
